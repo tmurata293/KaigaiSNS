b9e1e40271ae59ca09b425eea3e5cece3b77e4ad
Tsugumi Murata <tmurata@uwaterloo.ca>
2018-05-05 15:30:54 -0400

    Login_page_prototype1
    - email and password an textInput
    - added secure text to password
    - added keyboard avoid

--- KaigaiSNS_test/App.js
+++ KaigaiSNS_test/App.js
@@ -1,39 +1,57 @@
+/*************************************
+ ******** KaigaiSNS_login*************
+ *************************************
+ Creator: Tsugumi Murata *************
+ Creator:                *************
+ *************************************
+ *************************************/
+
+
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, AppRegistry, TouchableOpacity, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry, TouchableOpacity, Image, TextInput, KeyboardAvoidingView } from 'react-native';
 
 // component for APP login
 export default class app_login extends React.Component {
     constructor(props){
         super(props);
-        this.state = {text: ''};
+        this.state = {
+            email: '',
+            password: ''
+        };
     }
   render() {
       
     return (
-    <View style={login_UI.container}>
+    <View style={{flex:1}}>
             //画像はKaigaiSNS_test内のimageディレクトリにある
             <Image source = {require('./image/background_img.jpg')} style={login_UI.image}/>
             //app_ name
-        <View style = {{flex:0.8, justifyContent: 'flex-end', alignItems:'center'}}>
+        <View style = {{flex:1, justifyContent: 'flex-end', alignItems:'center'}}>
             <Text style = {login_UI.app_name} >KAIGAI-SNS</Text>
         </View>
+
+        <KeyboardAvoidingView style = {{flex: 2, flexDirection: 'row'}} behavior={"padding"} enabled>
             
-        <View style={login_UI.container2}>
             <View style = {{flex:1}} />
-            <View style = {{flex: 10, flexDirection:'column'}}>
-                    <View style = {{flex: 5}}/>
-                    <View style = {{flex: 1}}>
+            <View style = {{flex: 8, flexDirection:'column'}}>
+                    <View style = {{flex: 5}} />
+                    <View style = {[login_UI.text_column, {flex: 0.6, justifyContent: 'center'}]}>
                         // email column
-                        <TouchableOpacity onPress = {this._onPressButton} style = {login_UI.text_column}>
-                            <Text style={{fontFamily: 'Cochin', color: '#E0E0E0',  fontWeight: 'bold', fontSize: 20}}> Email </Text>
-                        </TouchableOpacity>
+                        <TextInput  placeholder = "Email"
+                                    onChangeText ={(email) => this.setState({email})}
+                                    style = {login_UI.text_input}
+                                    autoCorrect = {false} />
                     </View>
-                    <View style = {{flex: 1}}>
+                    <View style = {{flex:0.2}} />
+                    <View style = {[login_UI.text_column, {flex: 0.6, justifyContent: 'center'}]}>
                         // password column
-                        <TouchableOpacity onPress = {this._onPressButton} style = {login_UI.text_column}>
-                            <Text style={{fontFamily: 'Cochin' , color: '#E0E0E0',  fontWeight: 'bold', fontSize: 20}}> Password </Text>
-                        </TouchableOpacity>
+                        <TextInput  placeholder = "Password"
+                                    onChangeText ={(password) => this.setState({password})}
+                                    style = {login_UI.text_input}
+                                    secureTextEntry = {true}
+                                    autoCorrect = {false} />
                     </View>
+                    <View style = {{flex:0.2}} />
                     <View style = {{flex: 1, flexDirection: 'row'}}>
                         <View style = {{flex: 1}}>
                             // login button
@@ -63,7 +81,7 @@ export default class app_login extends React.Component {
                     </View>
             </View>
             <View style = {{flex:1}} />
-        </View>
+        </KeyboardAvoidingView>
         <View style = {{flex:0.05}}/>
             
     </View>
@@ -73,17 +91,7 @@ export default class app_login extends React.Component {
 
 
 const login_UI = StyleSheet.create({
-  container: {
-    flex: 1,
-    //justifyContent: 'center',
-  },
-                                   
-  container2: {
-  flex: 2,
-  flexDirection: 'row',
-  //justifyContent: 'center',
-  },
-                                   
+                            
   image:{
    
    width: '100%',
@@ -102,10 +110,9 @@ const login_UI = StyleSheet.create({
   },
                                    
   text_column:{
-     alignItems: 'center',
-     //position: 'absolute',
-     backgroundColor: '#ffffff',
-     borderRadius: 30,
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    borderRadius: 30,
   },
                                    
   button_small_1:{
@@ -123,6 +130,15 @@ const login_UI = StyleSheet.create({
   button_touch:{
       alignItems: 'center',
    },
+                                   
+   text_input:{
+       alignItems: 'center',
+       fontFamily: 'Cochin' ,
+       color: '#E0E0E0',
+       fontSize: 20,
+       fontWeight: 'bold',
+    },
+                                   
                                    
 });
 
